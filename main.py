0ed7765f8b325ade4d59e501b7faf0561c32b362
Randy Suarez Rodes <rsrjohnson@gmail.com>
2021-09-14 21:10:33 -0500

    add gas command, tracking gas work in progress

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from discord.ext import commands
 import os
 from firebase_admin import credentials, initialize_app, storage
 import pandas as pd
-import datetime
+
 import json
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+opts = Options()
+opts.add_argument('--no-sandbox')
+opts.add_argument('--disable-dev-shm-usage')
+#opts.add_argument('-headless')
+
+
+
+import datetime
+#from datetime import datetime
+#import schedule
+import time
 #import shutil
 #import urllib.request
 #import requests
@@ -17,8 +33,6 @@ cred = credentials.Certificate(json.loads(F_CRED))
 initialize_app(cred, {'storageBucket': 'quickhtmlhost.appspot.com'})
 
 os.system('pip install kaleido')
-
-
 
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
 import matplotlib.pyplot as plt
@@ -48,41 +62,33 @@ async def on_ready():
 @bot.command()
 async def p(ctx,message,currency='usd'):
 
-  coin_qry=message.lower()
+  qry=message.lower()
 
-  coin_id=df_gecko.loc[df_gecko['symbol']==coin_qry,'id'].iloc[0]
+  coin_idg=df_gecko.loc[df_gecko['symbol']==qry,'id'].iloc[0]
   #coin_id=re.findall(r'-+(.*)',coin_id)[0]
 
-  #icon='https://raw.githubusercontent.com/TokenTax/cryptoicon-api/master/public/icons/128/color/'+coin_qry+'.png' 
+  icon='https://raw.githubusercontent.com/rsrjohnson/Coin-PredaBot/main/icons/'+qry+'.png'
 
-  icon='https://raw.githubusercontent.com/rsrjohnson/Coin-PredaBot/main/icons/'+coin_qry+'.png'
+  curr_price=cg.get_price(ids=coin_idg, vs_currencies=currency.lower(),include_24hr_change=True)
 
-  curr_price=cg.get_price(ids=coin_id, vs_currencies=currency.lower(),include_24hr_change=True)
-
-  today=curr_price[coin_id][currency]
-  change=round(curr_price[coin_id][currency+'_24h_change'],4)
+  today=curr_price[coin_idg][currency]
+  change=round(curr_price[coin_idg][currency+'_24h_change'],4)
   
   #yest=today-change
   #percent=round(change/yest*100,2)
 
-  coloring=0xff0000
-  
+  coloring=0xff0000  
 
   if change > 0:  
     coloring=0x00ff00
     change='+'+str(change)
-   
-
-  #output='**price '+' '+ +'**\n' + '\n**24h change** '+' '+'**'+str(change)+'%**'
-  
-  embed = discord.Embed(title=coin_id,color=coloring)
+     
+  embed = discord.Embed(title=coin_idg,color=coloring)
   embed.add_field(name='price', value='**'+str(today) + ' '+ currency.upper()+'**', inline=False)
   embed.add_field(name='24h change', value='**'+str(change)+'%**', inline=False)
 
   embed.set_image(url=icon)
-  await ctx.send(embed=embed)
- 
-  #discord.Embed(title="Hey", description="How are you?", color=colors[color_name])  
+  await ctx.send(embed=embed) 
   
 
 
@@ -102,7 +108,6 @@ async def candle(ctx,message,number_of_days=1):
     number_of_days=int(number_of_days)
     
     coin_qry=message.upper()
-
     coin_id=df_paprika.loc[df_paprika['symbol']==coin_qry,'id'].iloc[0]
     #coin_id=re.findall(r'-+(.*)',coin_id)[0]
     
@@ -113,10 +118,11 @@ async def candle(ctx,message,number_of_days=1):
 
 
     for i in range(number_of_days):
-        date=(start_date + datetime.timedelta(days = i)).isoformat()
-        candles_data['data'].append(cl.candles(coin_id, start=date)[0])
+        date_=(start_date + datetime.timedelta(days = i)).isoformat()
+        candles_data['data'].append(cl.candles(coin_id, start=date_)[0])
 
-    df_candles = pd.json_normalize(candles_data['data'])
+    df_candles = pd.DataFrame(candles_data['data'])
+    df_candles.columns=['time_open','time_close','open','high','low','close','volume','market_cap']
 
     fig = go.Figure(data=[go.Candlestick(x=df_candles['time_open'],
                 open=df_candles['open'],
@@ -143,16 +149,62 @@ async def candle(ctx,message,number_of_days=1):
     await ctx.send(blob.public_url)
 
 @bot.command()
-async def gas(ctx,threshold):
+async def gas(ctx):
 
-  df_gas = pd.read_csv('gas_notes.csv',index_col=0)
-  #n=df_gas.shape[0]  
-  df_gas=df_gas.append({'threshold':[threshold],'user':[ctx.author.id]},ignore_index=True)
+  url = "https://etherscan.io/gastracker"
 
-  df_gas.to_csv('gas_notes.csv')  
+  driver = webdriver.Chrome(options=opts) 
 
-  await ctx.send('received')
+  driver.get(url)
+  # this is just to ensure that the page is loaded
+  time.sleep(5)
 
+  html = driver.page_source
+
+  # this renders the JS code and stores all
+  # of the information in static HTML code.
+
+  # Now, we could simply apply bs4 to html variable
+  soup = BeautifulSoup(html, "html.parser")
+  gas_low = soup.find('span', {'id' : 'spanLowPrice'})
+  gas_avg=soup.find('span',{'id':'spanAvgPrice'})
+  gas_high=soup.find('span',{'id':'spanHighPrice'})
+
+
+  driver.close()
+
+  low=list(gas_low.children)[0].contents[0]
+  avg=list(gas_avg.children)[0]
+  high=list(gas_high.children)[0]
+
+  await ctx.send('low: '+ low + ', avg: ' + avg + ', high: ' + high)
+
+@bot.command()
+async def gas(ctx):
+
+  url = "https://etherscan.io/gastracker"
+
+  driver = webdriver.Chrome(options=opts) 
+
+  driver.get(url)
+  # this is just to ensure that the page is loaded
+  time.sleep(5)
+
+  html = driver.page_source
+
+  soup = BeautifulSoup(html, "html.parser")
+  gas_low = soup.find('span', {'id' : 'spanLowPrice'})
+  gas_avg=soup.find('span',{'id':'spanAvgPrice'})
+  gas_high=soup.find('span',{'id':'spanHighPrice'})
+
+
+  driver.close()
+
+  low=list(gas_low.children)[0].contents[0]
+  avg=list(gas_avg.children)[0]
+  high=list(gas_high.children)[0]
+
+  await ctx.send('low: '+ low + ', avg: ' + avg + ', high: ' + high)
 
 @bot.command()
 async def test(ctx, arg):
